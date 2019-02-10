c7e0153f1e27bc787de4483369033c118287ccf0
Justus <47227577+Jonny2019@users.noreply.github.com>
2019-02-10 14:52:53 +0100

    Update home.php
    enbed in HTML

--- home.php
+++ home.php
@@ -1,5 +1,12 @@
-<?php 
-echo "Benutzername:". $_POST['UserName'];
-echo "Passwort:". $_POST['Password'];        
-?>
-                 
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+  </head>
+  <body>
+    <?php
+    echo "Benutzername:". $_POST['UserName'];
+    echo "Passwort:". $_POST['Password'];
+    ?>
+  </body>
+</html>
