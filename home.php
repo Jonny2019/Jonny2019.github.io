2e5ff0bcedc79f9450400c679618b45dd462ad38
Justus <47227577+Jonny2019@users.noreply.github.com>
2019-02-10 14:24:31 +0100

    Update home.php
    include only php

--- home.php
+++ home.php
@@ -1,27 +1,5 @@
-<!DOCTYPE html>
-<html lang="de" dir="ltr">
-    <head>
-        <meta charset="utf-8"/>
-        <title>Grenzweg</title>
-        <meta http-equiv="refresh" content="1"/>
-        <meta http-equiv="cache-control" content="no cache"/>
-        <link rel="stylesheet" type="text/css" href="home.css" charset="utf-8"/>
-    </head>
-    <body>
-        <div class="AllContentWrapper">
-            <div class="login">
-                <?php 
-                    echo "Benutzername:". $_POST['UserName'];
-                    echo "Passwort:". $_POST['Password'];        
-                 ?>
-                 <form action="home.php" method="post">
-                    <h1>Benutzername</h1>
-                    <input type="text" name="UserName"/>
-                    <h1>Password</h1>
-                    <input type="password" name="Password"/>
-                    <input type="submit" value="absenden"/>
-                 </form>
-            </div>
-        </div>
-    </body>
-</html>
+<?php 
+echo "Benutzername:". $_POST['UserName'];
+echo "Passwort:". $_POST['Password'];        
+?>
+                 
