cb1085c2e8feaef582b018004cfe80e4131cb79a
Justus <47227577+Jonny2019@users.noreply.github.com>
2019-03-02 20:04:39 +0100

    Update index.js
    correct link for "Grenzweg"

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ document.getElementById('Links').addEventListener('click', links);
 document.getElementById('Home').addEventListener('click', home);
 var AboutMe = "<h1>about me</h1><p>I am very interested in coding and science! So have a look at the Downloads and have fun with them!</p>";
 var Downloads = "<h1>Downloads</h1><ul><li><a href='Elementarteilchen.pyw' download>Elementarteilchen</a></li><li><a href='Homepage' download>Blue Bird</a></li></ul>"
-var Links = "<h1>Links</h1><a href='grenzweg-wb.bplaced.net'>Grenzweg</a>"
+var Links = "<h1>Links</h1><a href='https://grenzweg-wb.bplaced.net'>Grenzweg</a>"
 var Home = "<h1>Home</h1><p>Welcome to my Homepage!<br/>Explore and have fun!</p>"
 function aboutme()
 {
